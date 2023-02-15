87a07d9d865ad73fbbf968f060beb02a10bbb26e
AnchovyLuck <tiencacom20@gmail.com>
2023-02-15 19:54:44 +0700

    Create palindrome.js
    Add a function.

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+function isAlphanumeric(str) {
+    return str.length === 1 && str.match(/[a-z0-9]/gi);
+  }
+  function palindrome(str) {
+    for (let i = 0; i < str.length; i++) {
+      if (!isAlphanumeric(str[i])) {
+        str = str.replace(str[i], "");
+        i--;
+      }
+    }
+    str = str.toLowerCase();
+    for (let i = 0; i < str.length / 2; i++) {
+      if (str[i] !== str[str.length - i - 1]) {
+        return false;
+      }
+    }
+    return true;
+}
+let testString = "_eye";
+console.log(palindrome(testString));
